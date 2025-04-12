7ad6b638f87f186e8fe327f76e4940dfebabea6b
İsmail Baran <ismailbaran04@gmail.com>
2025-04-12 17:32:28 +0300

    sayfa viewcomponentlere ayrıldı, devam edicek

---
+++ TraversalCoreProject/ViewComponents/UILayout/_UILayoutScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.UILayout
+{
+    public class _UILayoutScriptComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
