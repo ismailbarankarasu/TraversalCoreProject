5eea6029f26d71a34e16b2e5abde954a41c5307c
İsmail Baran <ismailbaran04@gmail.com>
2025-05-03 22:16:00 +0300

    indexde veriler listelendi ve details sayfası oluşturuldu

---
+++ TraversalCoreProject/Controllers/DestinationController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class DestinationController : Controller
+    {
+        private readonly IDestinationService _destinationService;
+
+        public DestinationController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _destinationService.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult DestinationDetails(int id) 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination p)
+        {
+            return View();
+        }
+    }
+}
