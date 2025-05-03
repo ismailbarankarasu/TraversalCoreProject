266711285ad222dde9e16e1b906b2c78da6aae39
İsmail Baran <ismailbaran04@gmail.com>
2025-05-03 22:13:49 +0300

    otomatik güncellemeler işlendi

--- EntityLayer/obj/Debug/net5.0/EntityLayer.AssemblyInfo.cs
+++ EntityLayer/obj/Debug/net5.0/EntityLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaeb256faa92fc3fdac018f1ec5493b651850a00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa56485070d234ecbbc256d34cf757ed4e311c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
