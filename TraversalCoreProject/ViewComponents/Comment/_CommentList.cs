b89c232e1f818b3630634c7e85dc6b9b4b360cb6
İsmail Baran <ismailbaran04@gmail.com>
2025-05-04 23:53:28 +0300

    comment eklendi

---
+++ TraversalCoreProject/ViewComponents/Comment/_CommentList.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Comment
+{
+    public class _CommentList:ViewComponent
+    {
+        private readonly ICommentService _commentService;
+
+        public _CommentList(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = _commentService.TGetDestinationById(id);    
+            return View(values);
+        }
+    }
+}
