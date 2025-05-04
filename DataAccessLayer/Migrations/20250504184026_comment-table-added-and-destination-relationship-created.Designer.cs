f7d79d47a0775be442dc4bcde9620c9142c333c0
İsmail Baran <ismailbaran04@gmail.com>
2025-05-04 23:48:28 +0300

    migraiton alındı

--- DataAccessLayer/Migrations/20250504184026_comment-table-added-and-destination-relationship-created.Designer.cs
+++ DataAccessLayer/Migrations/20250504184026_comment-table-added-and-destination-relationship-created.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250504184026_comment-table-added-and-destination-relationship-created")]
+    partial class commenttableaddedanddestinationrelationshipcreated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
