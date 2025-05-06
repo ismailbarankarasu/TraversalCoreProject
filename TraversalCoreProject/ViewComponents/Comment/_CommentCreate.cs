4fac361fe933f3a132b2adac010060878f5d94b7
İsmail Baran <ismailbaran04@gmail.com>
2025-05-06 21:21:53 +0300

    yorum yapma kısmı yorumlar ve yorum yap olarak 2 ye ayrıldı

--- TraversalCoreProject/ViewComponents/Comment/_CommentCreate.cs
+++ TraversalCoreProject/ViewComponents/Comment/_CommentCreate.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace TraversalCoreProject.ViewComponents.Comment
 {
     public class _CommentCreate:ViewComponent
     {
+        private readonly ICommentService _commentService;
+
+        public _CommentCreate(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+        [HttpGet]
         public IViewComponentResult Invoke()
         {
             return View();
