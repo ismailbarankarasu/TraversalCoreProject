29e30f0f02f708ce3074a7d68b457953429765ff
İsmail Baran <ismailbaran04@gmail.com>
2025-05-06 21:23:59 +0300

    comment controllerda _createcomment viewcomponent deki gelen sayfanın post işlemi yapıldı

---
+++ TraversalCoreProject/Controllers/CommentController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpPost]
+        public IActionResult CommentCreate(Comment p)
+        {
+            p.CommentDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.CommentState = true;
+            _commentService.TAdd(p);
+            return RedirectToAction("Index", "Destination");
+        }
+    }
+}
