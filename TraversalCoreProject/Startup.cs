a59d2d9f3c6b0b1f516d1d85f6a757425491fcb9
İsmail Baran <ismailbaran04@gmail.com>
2025-05-08 17:06:56 +0300

    member endpoint eklendi

--- TraversalCoreProject/Startup.cs
+++ TraversalCoreProject/Startup.cs
@@ -89,6 +89,13 @@ namespace TraversalCoreProject
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
