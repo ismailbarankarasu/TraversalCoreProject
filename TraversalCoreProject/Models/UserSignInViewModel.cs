5dc14bdf7954ad9e6bc47f8964971e41990e8128
İsmail Baran <ismailbaran04@gmail.com>
2025-05-08 17:08:18 +0300

    giriş yapmak için view model oluşturuldu

---
+++ TraversalCoreProject/Models/UserSignInViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProject.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage ="Lütfen Kullanıcı Adınız Giriniz!")]
+        public string Username { get; set; }
+        [Required(ErrorMessage ="Lütfen Şifrenizi Giriniz")]
+        public string Password { get; set; }
+    }
+}
