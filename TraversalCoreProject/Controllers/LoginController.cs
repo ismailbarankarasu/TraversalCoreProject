9d88e869aa107072825dd21d5df7805637237344
İsmail Baran <ismailbaran04@gmail.com>
2025-05-08 17:09:41 +0300

    Login controllerda signin tamamlandı

--- TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -11,10 +11,12 @@ namespace TraversalCoreProject.Controllers
     public class LoginController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public LoginController(UserManager<AppUser> userManager)
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [HttpGet]
@@ -54,6 +56,22 @@ namespace TraversalCoreProject.Controllers
         {
             return View();
         }
-
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserSignInViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.Username, p.Password, false, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Profile", new { area = "Member" });
+                }
+                else
+                {
+                    return RedirectToAction("SignIn", "Login");
+                }
+            }
+            return View();
+        }
     }
 }
