8b110a9e50d89bed9e779341bfff4e5349431dd8
İsmail Baran <ismailbaran04@gmail.com>
2025-05-09 20:14:53 +0300

    yeni rezervasyoon oluşturma kodları yazıldı

--- TraversalCoreProject/Areas/Member/Controllers/ProfileController.cs
+++ TraversalCoreProject/Areas/Member/Controllers/ProfileController.cs
@@ -1,6 +1,8 @@
 ﻿using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using TraversalCoreProject.Areas.Member.Models;
 
@@ -26,6 +28,32 @@ namespace TraversalCoreProject.Areas.Member.Controllers
             userEditViewModel.phoneNumber = values.PhoneNumber;
             userEditViewModel.mail = values.Email;
             return View(userEditViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Image != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(p.Image.FileName);
+                var newImageName = Guid.NewGuid() + extension;
+                var saveLocation = resource + "/wwwroot/UserImages/" + newImageName;
+                var stream = new FileStream(saveLocation, FileMode.Create);
+                await p.Image.CopyToAsync(stream);
+                user.ImageUrl = newImageName;
+            }
+            user.Name = p.name;
+            user.Surname = p.surname;
+            user.PhoneNumber = p.phoneNumber;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, p.password);
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("SignIn", "Login");
+            }
+            return View();
         }
     }
 }
---
+++ TraversalCoreProject/Areas/Member/Controllers/ReservationController.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class ReservationController : Controller
+    {
+        private readonly IDestinationService _destinationService;
+
+        public ReservationController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        public IActionResult MyCurrentReservation()
+        {
+            return View();
+        }
+        public IActionResult MyOldReservation()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult NewReservation()
+        {
+            List<SelectListItem> values = (from x in _destinationService.TGetList()
+                        select new SelectListItem
+                        {
+                            Text = x.City,
+                            Value = x.DesctinationId.ToString()
+                        }).ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewReservation(Reservation p)
+        {
+            return View();
+        }
+    }
+}
