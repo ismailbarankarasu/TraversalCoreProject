d8369adb26d678983c20e13e01fb2df34d7d57fd
İsmail Baran <ismailbaran04@gmail.com>
2025-05-12 23:43:54 +0300

    otomatik güncellemeler işlendi

--- TraversalCoreProject/obj/Debug/net5.0/TraversalCoreProject.RazorTargetAssemblyInfo.cs
+++ TraversalCoreProject/obj/Debug/net5.0/TraversalCoreProject.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("TraversalCoreProject")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9d9f56f7bbbfeb0dd27f6ba6ee46920baa312f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4519da5bc35636580ac16ae9e73af3e4926cbe7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TraversalCoreProject.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
