fb6e72fa0e4aab56b54e7ba958c164fb29e55281
İsmail Baran <ismailbaran04@gmail.com>
2025-05-12 23:44:54 +0300

    admin area sı oluşturuldu, destination controller üzerinden view oluşturuldu ve crud işlemleri tamamlandı

---
+++ TraversalCoreProject/Areas/Admin/Controllers/DestinationController.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DestinationController : Controller
+    {
+        private readonly IDestinationService _destinationService;
+
+        public DestinationController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _destinationService.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddDestination()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            _destinationService.TAdd(destination);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteDestination(int id)
+        {
+            var value = _destinationService.TGetById(id);
+            _destinationService.TDelete(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateDestination(int id)
+        {
+            var value = _destinationService.TGetById(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            _destinationService.TUpdate(destination);
+            return RedirectToAction("Index");
+        }
+    }
+}
