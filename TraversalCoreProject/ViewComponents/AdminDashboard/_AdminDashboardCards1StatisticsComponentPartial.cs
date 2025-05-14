f418319f4788be2082540750c3bd0255a6442860
İsmail Baran <ismailbaran04@gmail.com>
2025-05-14 17:24:56 +0300

    admin dashboard component classları eklendi

---
+++ TraversalCoreProject/ViewComponents/AdminDashboard/_AdminDashboardCards1StatisticsComponentPartial.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace TraversalCoreProject.ViewComponents.AdminDashboard
+{
+    public class _AdminDashboardCards1StatisticsComponentPartial:ViewComponent
+    {
+        private readonly IDestinationService _destinationService;
+
+        public _AdminDashboardCards1StatisticsComponentPartial(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.destinationCount = _destinationService.TDestinationCount();
+            ViewBag.guideCount = _destinationService.TUsersCount();
+            return View();
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/AdminDashboard/_AdminDashboardCards2StatisticsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.AdminDashboard
+{
+    public class _AdminDashboardCards2StatisticsComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
