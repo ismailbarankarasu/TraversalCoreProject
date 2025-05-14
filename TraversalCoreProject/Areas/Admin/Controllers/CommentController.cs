336cfe32f5820f6c7266a639eba3ea65cb918d71
İsmail Baran <ismailbaran04@gmail.com>
2025-05-14 17:24:01 +0300

    admin areas da comment ve user tamamlandı

---
+++ TraversalCoreProject/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CommentController : Controller
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _commentService.TCommentListWithDestination();
+            return View(values);
+        }
+        public IActionResult DeleteComment(int id)
+        {
+            var value = _commentService.TGetById(id);
+            _commentService.TDelete(value);
+            return RedirectToAction("Index", "Comment", new { area = "Admin" });
+
+        }
+    }
+}
---
+++ TraversalCoreProject/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IAppUserService _appUserService;
+
+        public UserController(IAppUserService appUserService)
+        {
+            _appUserService = appUserService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _appUserService.TGetList();
+            return View(values);
+        }
+        public IActionResult DeleteUser(int id)
+        {
+            var value = _appUserService.TGetById(id);
+            _appUserService.TDelete(value);
+            return RedirectToAction("Index", "User", new { area = "Admin" });
+        }
+        [HttpGet]
+        public IActionResult EditUser(int id)
+        {
+            var values = _appUserService.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]  
+        public IActionResult EditUser(AppUser appUser)
+        {
+            _appUserService.TUpdate(appUser);
+            return RedirectToAction("Index", "User", new { area = "Admin" });
+        }
+        public IActionResult CommentUser(int id)
+        {
+            return View();
+        }
+        public IActionResult ReservationUser(int id)
+        {
+            return View();
+        }
+    }
+}
