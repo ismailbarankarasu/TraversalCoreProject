f9152a0a10228cbe52882a90667c5caf16fec610
İsmail Baran <ismailbaran04@gmail.com>
2025-05-19 23:19:16 +0300

    rehber işlemleri tamamlandı

---
+++ TraversalCoreProject/Areas/Admin/Controllers/GuideController.cs
@@ -0,0 +1,82 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.ValidationRules;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GuideController : Controller
+    {
+        private readonly IGuideService _guideService;
+
+        public GuideController(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _guideService.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddGuide()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddGuide(Guide guide)
+        {
+            GuideValidator validationRules = new GuideValidator();
+            FluentValidation.Results.ValidationResult result = validationRules.Validate(guide);
+            if (result.IsValid)
+            {
+                _guideService.TAdd(guide);
+                return RedirectToAction("Index", "Guide", new { Areas = "Admin" });
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
+        }
+        public IActionResult DeleteGuide(int id)
+        {
+            var values = _guideService.TGetById(id);
+            _guideService.TDelete(values);
+            return RedirectToAction("Index", "Guide", new { Areas = "Admin" });
+        }
+        [HttpGet]
+        public IActionResult EditGuide(int id)
+        {
+            var values = _guideService.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditGuide(Guide guide)
+        {
+            _guideService.TUpdate(guide);
+            return RedirectToAction("Index", "Guide", new { Areas = "Admin" });
+        }
+        public IActionResult StatusGuide(int id)
+        {
+            var values = _guideService.TGetById(id);
+            if (values.Status == true)
+            {
+                values.Status = false;
+            }
+            else
+            {
+                values.Status = true;
+            }
+            _guideService.TUpdate(values);
+            return RedirectToAction("Index", "Guide", new { Areas = "Admin" });
+        }
+
+    }
+}
