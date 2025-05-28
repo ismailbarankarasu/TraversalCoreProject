ac76a0ad87cb4a85e4feeb3c49ed0cae80738739
İsmail Baran <ismailbaran04@gmail.com>
2025-05-29 00:38:49 +0300

    city bilgisini statik olarak çekebilmek için bir adet model oluşturuldu

---
+++ TraversalCoreProject/Models/CityClass.cs
@@ -0,0 +1,9 @@
+﻿namespace TraversalCoreProject.Models
+{
+    public class CityClass
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public string Country { get; set; }
+    }
+}
