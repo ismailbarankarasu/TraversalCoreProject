4fd019431fc646cf68e9d89c83960090a16bc0b1
İsmail Baran <ismailbaran04@gmail.com>
2025-06-09 23:56:05 +0300

    otomatik güncellemeler işlendi

--- BusinessLayer/obj/Debug/net5.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net5.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e831d0ad660552cef6a5ddcb4741fe816d1c6aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3277e06efe451301594e73825775b3efc7b6228f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
