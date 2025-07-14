972bb35aa28f4645a5421cf2cfd0f463a50a2b5f
İsmail Baran <ismailbaran04@gmail.com>
2025-07-14 23:48:15 +0300

    kullanıcı kayıt için özel hata mesajları fluentvalidation ile eklendi

---
+++ BusinessLayer/ValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,27 @@
+﻿using DTOLayer.DTOs.AppUserDTOs;
+using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AppUserRegisterValidator:AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad Alanı Boş Geçilmez !");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad Alanı Boş Geçilmez !");
+            RuleFor(x => x.Mail).NotEmpty().WithMessage("Mail Alanı Boş Geçilmez !");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı Adı Alanı Boş Geçilmez !");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre Alanı Boş Geçilmez !");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre Tekrar Alanı Boş Geçilmez !");
+            RuleFor(x => x.Username).MinimumLength(5).WithMessage("Lütfen En Az 5 Karakter Veri Girişi Yapınız !");
+            RuleFor(x => x.Username).MaximumLength(20).WithMessage("Lütfen En Fazla 20 Karakter Veri Girişi Yapınız !");
+            RuleFor(x => x.Password).Equal(y => y.ConfirmPassword).WithMessage("Şifreler Birbiriyle Uyuşmuyor");
+        }
+    }
+}
