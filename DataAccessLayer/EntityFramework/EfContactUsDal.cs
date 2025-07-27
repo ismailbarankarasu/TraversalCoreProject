47dcc3330530a8579d7dc1edd37d794342a78c6a
İsmail Baran <ismailbaran04@gmail.com>
2025-07-28 00:32:56 +0300

    contact us için gerekli classlar ve interfaceler oluşturuldu ardından gelen mesajları false ve true ya çevirme kodları eklendi

---
+++ DataAccessLayer/EntityFramework/EfContactUsDal.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfContactUsDal : GenericRepository<ContactUs>, IContactUsDal
+    {
+        private readonly Context _context;
+        public EfContactUsDal(Context context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void ContactUseStatusChangeToFalse(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ContactUs> GetListContactUsByFalse()
+        {
+            var values = _context.ContactUses.Where(x => x.MessageStatus == false)
+                .ToList();
+            return values;
+        }
+
+        public List<ContactUs> GetListContactUsByTrue()
+        {
+            var values = _context.ContactUses.Where(x => x.MessageStatus == true)
+                .ToList();
+            return values;
+        }
+    }
+}
