9dcd7eb9a144ffca4c806d84f2604bbef4def50f
İsmail Baran <ismailbaran04@gmail.com>
2025-08-01 21:17:14 +0300

    announcement için mapleme yapıldı

---
+++ TraversalCoreProject/Mapping/AutoMapperProfile/MapProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DTOLayer.DTOs.AnnouncementDTOs;
+using DTOLayer.DTOs.AppUserDTOs;
+using DTOLayer.DTOs.CityDTOs;
+using EntityLayer.Concrete;
+
+namespace TraversalCoreProject.Mapping.AutoMapperProfile
+{
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<AnnouncementAddDto, Announcement>().ReverseMap();
+            CreateMap<AppUserRegisterDto, AppUser>().ReverseMap();
+            CreateMap<AppUserLoginDto, AppUser>().ReverseMap();
+        }
+    }
+}
